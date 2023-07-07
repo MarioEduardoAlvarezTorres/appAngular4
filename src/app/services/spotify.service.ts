b91c7240e9c8725b032613ce7e01499ee6a3c57d
Mario Eduardo <eduardoat44@gmail.com>
2023-07-07 17:19:39 -0600

    centralizar las peticiones hacia spotify

--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -9,30 +9,26 @@ import { map } from 'rxjs/operators';
 export class SpotifyService {
   constructor(private http: HttpClient) {}
 
-  getNewReleases() {
+  getQuery(query: string) {
+    const url = `https://api.spotify.com/v1/${query}`;
+
     const headers = new HttpHeaders({
       Authorization:
         'Bearer BQBr96h4_re2uTubhMBRa5uzGNmfqHeAYgQvWHc5tI2xPE17d2EjomZMenqso67YOVGZRLsRsSG99-3uBlE1WWw6jt4O0AkDIfs49ipCJka_gKqDeFg',
     });
-    return this.http
-      .get('https://api.spotify.com/v1/browse/new-releases', {
-        headers,
-      })
-      .pipe(map((data: any) => data['albums'].items));
+
+    return this.http.get(url, { headers });
+  }
+
+  getNewReleases() {
+    return this.getQuery('browse/new-releases').pipe(
+      map((data: any) => data['albums'].items)
+    );
   }
 
   getSearch(termino: string) {
-    const headers = new HttpHeaders({
-      Authorization:
-        'Bearer BQBr96h4_re2uTubhMBRa5uzGNmfqHeAYgQvWHc5tI2xPE17d2EjomZMenqso67YOVGZRLsRsSG99-3uBlE1WWw6jt4O0AkDIfs49ipCJka_gKqDeFg',
-    });
-    return this.http
-      .get(
-        `https://api.spotify.com/v1/search?q=${termino}&type=artist%2Calbum%2Ctrack`,
-        {
-          headers,
-        }
-      )
-      .pipe(map((data: any) => data['artists'].items));
+    return this.getQuery(
+      `search?q=${termino}&type=artist%2Calbum%2Ctrack`
+    ).pipe(map((data: any) => data['albums'].items));
   }
 }
